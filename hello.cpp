29c7c625152a86cf8bf1c01c395cab4980cabd60
pratikExpOS <npratik10@gmail.com>
2018-06-02 23:55:30 -0700

    Adding hello world code

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+   int a = 4, b = 5;
+   int sum = 0;
+   sum = a + b;
+   std::cout << "Sum : " << sum << "\n";
+   std::cout << "hello world" << "\n";
+   return 0;
+}
